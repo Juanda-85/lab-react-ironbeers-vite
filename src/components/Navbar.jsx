b34e46bfae6c8fcfc32bf7d221cfab0ba38e5d81
Juan David Valencia <juandavalg@gmail.com>
2023-09-12 22:41:31 +0200

    IronBeers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,11 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import home from "./../assets/home-icon.png"
+
+function Navbar() {
+    return (
+        <Link to="/"><img src={home} alt="home" style={{backgroundColor: "blue"}}/></Link>
+
+    )
+}
 
 export default Navbar;
--- src/pages/AllBeersPage.jsx
+++ src/pages/AllBeersPage.jsx
@@ -1,3 +1,48 @@
-function AllBeersPage() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+
+function AllBeersPage() {
+
+    const apiURL = "https://ih-beers-api2.herokuapp.com/beers";
+
+    const[beers, setBeers] = useState([])
+
+    useEffect(() => {
+        fetch(apiURL)
+        .then((res) => {
+            return res.json ()
+        })
+        .then((data)=> {
+            return setBeers(data)
+        })
+
+        .catch((err) => {
+            console.log(err)
+        })
+
+    }, [apiURL])
+    
+    console.log(beers)
+
+    return (
+        <>
+        {
+            beers.map((beer) => {
+                return (
+                <div>
+                    <Link to={`/beers/${beer._id}`}>
+                    <img src={beer.image_url} alt={beer.name} />
+                    <p>{beer.name}</p>
+                    <p>{beer.tagline}</p>
+                    <p>{beer.contributed_by}</p>
+                    </Link>
+                </div>
+                )
+            })
+        }
+        </>
+    )
+}
 
 export default AllBeersPage;
